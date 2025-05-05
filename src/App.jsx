705600d29f02f99611aa81adaea3cb12d3814de5
Moamen_Alghareeb <moamenalghareeb10@gmail.com>
2025-05-05 10:36:31 +0300

    adding themes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-const App = () => {
-  return (
-    <div className=''>Hello World</div>
-  )
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
 
-export default App
+export default App;
