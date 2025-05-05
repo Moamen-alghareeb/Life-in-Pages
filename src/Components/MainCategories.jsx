d32fba04bf8acf8061b0eeab1b29b38cde0d5720
Moamen_Alghareeb <moamenalghareeb10@gmail.com>
2025-05-05 18:42:52 +0300

    edited Categories to delete search bar

--- src/Components/MainCategories.jsx
+++ src/Components/MainCategories.jsx
@@ -3,39 +3,37 @@ import { Link } from 'react-router';
 
 export default function MainCategories() {
   return (
-    <div className="hidden md:flex text-my-black bg-my-active rounded-4xl xl:rounded-full p-4 shadow-xl items-center justify-between">
+    <div className="hidden md:flex items-center justify-around flex-wrap text-my-black bg-my-active rounded-4xl xl:rounded-full p-2 shadow-xl ">
       {/* Categories */}
-      <div className="flex-1 items-center justify-between flex-wrap">
-        <Link to="/posts" className="bg-my-hover rounded-3xl p-2">
-          All Posts
-        </Link>
-        <Link
-          to="/posts?cat=web-design"
-          className="hover:bg-my-hover rounded-3xl p-2 ml-1"
-        >
-          Web Design
-        </Link>
-        <Link
-          to="/posts?cat=web-design"
-          className="hover:bg-my-hover rounded-3xl p-2 ml-1"
-        >
-          Development
-        </Link>
-        <Link
-          to="/posts?cat=web-design"
-          className="hover:bg-my-hover rounded-3xl p-2 ml-1"
-        >
-          DB
-        </Link>
-        <Link
-          to="/posts?cat=web-design"
-          className="hover:bg-my-hover rounded-3xl p-2 ml-1"
-        >
-          SEO
-        </Link>
-      </div>
-      {/* search */}
-      <div className="">search</div>
+      {/* <div className="flex-1 items-center justify-between flex-wrap"> */}
+      <Link to="/posts" className="bg-my-hover rounded-3xl p-2">
+        All Posts
+      </Link>
+      <Link
+        to="/posts?cat=web-design"
+        className="hover:bg-my-hover rounded-3xl p-2 ml-1"
+      >
+        Web Design
+      </Link>
+      <Link
+        to="/posts?cat=web-design"
+        className="hover:bg-my-hover rounded-3xl p-2 ml-1"
+      >
+        Development
+      </Link>
+      <Link
+        to="/posts?cat=web-design"
+        className="hover:bg-my-hover rounded-3xl p-2 ml-1"
+      >
+        DB
+      </Link>
+      <Link
+        to="/posts?cat=web-design"
+        className="hover:bg-my-hover rounded-3xl p-2 ml-1"
+      >
+        SEO
+      </Link>
+      {/* </div> */}
     </div>
   );
 }
